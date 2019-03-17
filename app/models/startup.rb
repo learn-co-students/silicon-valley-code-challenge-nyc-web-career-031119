2b30869785931ec200c2cfcb7ecab526c1bdedff
github username <github email address>
2019-03-17 14:39:11 -0400

    added Startup class

--- app/models/startup.rb
+++ app/models/startup.rb
@@ -1,3 +1,35 @@
 class Startup
+  attr_reader :founder, :domain
+  attr_accessor :name
 
+  @@all = []
+  @@domain = []
+
+  def initialize(name, founder, domain)
+    @name = name
+    @founder = founder
+    @domain = domain
+    @@all << self
+    @@domain << domain
+  end
+
+  def pivot(domain, name)
+    self.name = name
+    self.domain = domain
+    @@domain << domain
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.find_by_founder(founder)
+    all.select do
+      |startup| startup.founder == self.founder
+    end
+  end
+
+  def self.domains
+    @@domain.uniq
+  end
 end
