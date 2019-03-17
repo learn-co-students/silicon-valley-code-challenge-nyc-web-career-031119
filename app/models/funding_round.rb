4aaa2ebb121381c53c87132281f1ef25653aa10e
github username <github email address>
2019-03-17 14:49:10 -0400

    added FundingRound class

--- app/models/funding_round.rb
+++ app/models/funding_round.rb
@@ -1,3 +1,13 @@
 class FundingRound
+  attr_reader :startup, :venture_capitalist
 
+  @@all = []
+
+  def initialize(startup, venture_capitalist, type, investment)
+    @startup = startup
+    @venture_capitalist = venture_capitalist
+    @type = type
+    @investment = investment
+    @@all << self
+  end
 end
