13c8651a5ce5e0aa69391ab47acf378da5e8be46
Theo Weller <weller.theodor@gmail.com>
2019-03-19 13:23:57 -0400

    Done

--- app/models/venture_capitalist.rb
+++ app/models/venture_capitalist.rb
@@ -1,3 +1,45 @@
 class VentureCapitalist
+  @@all = []
+attr_accessor :name, :total_worth
 
+def initialize(name, total_worth)
+  @name = name
+  @total_worth = total_worth
+  @@all << self
+end
+
+def self.all
+  @@all
+end
+
+def self.tres_commas_club
+  	VentureCapitalist.all.select do |person|
+		person.total_worth > 1000000000
+  	end
+end
+
+def offer_contract(startup, type, investment)
+	FundingRound.new(startup, self, type, investment)
+end
+
+def portfolio
+	FundingRound.all.map do |round|
+		round.venture_capitalist == self
+		round.startup
+	end.uniq
+end
+
+def biggest_investment
+	FundingRound.all.max_by do |round|
+		round.venture_capitalist == self
+		round.investment
+	end
+end
+
+def invested
+		FundingRound.all.map do |round|
+			round.venture_capitalist == self
+				round.investment
+		end.sum
+	end
 end
--- tools/console.rb
+++ tools/console.rb
@@ -7,5 +7,27 @@ end
 # This is a convenient place to define variables and/or set up new object instances,
 # so they will be available to test and play around with in your console
 
+#Startup
+boingo = Startup.new("Boingo", "Mr. Pebblerot", "Tech")
+tech_tongues = Startup.new("tech tongues", "Scott Sweatpants", "Tech")
+mr_hollywood = Startup.new("Mr. Hollywood", "Sarah Stutterbottom", "Travel")
+
+#venture_capitalist
+warren = VentureCapitalist.new("Warren", 10000000000)
+two_chains = VentureCapitalist.new("2Chainz", 900)
+basos = VentureCapitalist.new("El Jeffe Basos", 50000000000)
+
+basos.offer_contract(tech_tongues, "Angel", 400000)
+basos.offer_contract(tech_tongues, "Angel", 99999999999999)
+
+#FundingRound
+boingo.sign_contract(two_chains, "Angel", 100000)
+boingo.sign_contract(basos, "Angel", 500000)
+boingo.sign_contract(basos, "Angel", 10000)
+# boingo.FundingRound.new(boingo, warren, "Angel", 100000)
+# round_two = FundingRound.new(boingo, two_chains, "Pre-Seed", 50000)
+# round_one_tt = FundingRound.new(tech_tongues, basos,"Angel", 6000)
+
+
 binding.pry
 0 #leave this here to ensure binding.pry isn't the last line
