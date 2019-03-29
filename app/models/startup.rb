9ac66dae0a1b642249c091ca0dfb1b0e3477878e
Yevgeniy <yevgeniyerot@gmail.com>
2019-03-29 16:00:40 -0400

    Finished

--- app/models/startup.rb
+++ app/models/startup.rb
@@ -1,13 +1,33 @@
 class Startup
 
-  attr_accessor :name, :founder
+  attr_accessor :name, :founder, :domains
   attr_reader :domain
+  @@all = []
 
-  def initialize(name:, founder:)
-
+  def initialize(name, founder, domain)
+    @name = name
+    @founder = founder
+    @domain = domain
+    @domains << domain
+    @@all << self
   end
 
-  def pivot(domain:, name:)
-    
+  def pivot(domain, name)
+    @domain = domain
+    @name = name
+  end
+
+  def self.find_by_founder(name)
+    all.select do |startup|
+      startup.name == name
+    end
+  end
+
+  def self.domains
+    @domains
+  end
+
+  def self.all
+    @@all
   end
 end
